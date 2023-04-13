570d8b26a718ddc5e1ecf9c694b5aff77adeeaf0
Hisdaele <hisdaele.kamga@gmail.com>
2023-04-13 12:14:00 +0000

    initialize intellij project

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.util.Observable;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to design patterns !");
+    }
+}
